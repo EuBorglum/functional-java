01ea7607a0775faca88e1ad5b675e1dc5c313f86
tomborglum <tom.borglum@gmail.com>
2022-10-22 22:47:06 +0200

    setup-sonar - Updated ToBeImplemented

--- src/main/java/eu/borglum/functional/ToBeImplemented.java
+++ src/main/java/eu/borglum/functional/ToBeImplemented.java
@@ -2,10 +2,6 @@ package eu.borglum.functional;
 
 public class ToBeImplemented {
 
-    public static void main(String[] args) {
-        System.out.println(new ToBeImplemented().getMessage());
-    }
-
     public String getMessage() {
         return "To be implemented";
     }
