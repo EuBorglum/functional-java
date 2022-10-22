766234181e5cb7f5f21ad29a20b9932fb1f763e7
tomborglum <tom.borglum@gmail.com>
2022-10-22 22:12:10 +0200

    setup-sonar - Added junit5 - Added surefire - Added ToBeImplementedTest

--- src/main/java/eu/borglum/functional/ToBeImplemented.java
+++ src/main/java/eu/borglum/functional/ToBeImplemented.java
@@ -3,6 +3,10 @@ package eu.borglum.functional;
 public class ToBeImplemented {
 
     public static void main(String[] args) {
-        System.out.println("To be implemented");
+        System.out.println(new ToBeImplemented().getMessage());
+    }
+
+    public String getMessage() {
+        return "To be implemented";
     }
 }
